50f18ec81aa55e09cfcd3c11f8129aad179bdd8c
Shu Gen <gen_shu@caddi.jp>
2022-05-07 00:59:43 +0900

    remove unused import

--- backend/src/api/graphql/animal/animal.resolver.ts
+++ backend/src/api/graphql/animal/animal.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, ID, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import { Args, ID, Query, Resolver } from "@nestjs/graphql";
 import { AnimalDto } from "src/application/animal/animal.dto";
 import { AnimalService } from "src/application/animal/animal.service";
 
